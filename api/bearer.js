3c273e37ba4fa2a0019f1c596d5e5c22426497e9
Richard Haines <hello@richardhaines.dev>
2021-08-26 20:27:08 +0200

    Added encryption

--- api/bearer.js
+++ api/bearer.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
+const CryptoJS = require("crypto-js");
+
+const encryptWithAES = (stringToEncrypt) => {
+  return CryptoJS.AES.encrypt(stringToEncrypt, process.env.SECRET).toString();
+};
+
 module.exports = (req, res) => {
   res.json({
-      bearer: process.env.BEARER
+    bearer: encryptWithAES(process.env.BEARER)
   })
 }
