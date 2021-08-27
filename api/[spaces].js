906ed72d71f165fd09ee942d0e3d2be259568da7
Richard Haines <hello@richardhaines.dev>
2021-08-27 08:25:09 +0200

    Moved logic to serverless function

---
+++ api/[spaces].js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const fetch = require('node-fetch')
+
+
+const SPACES_URL = 'https://api.twitter.com/2/spaces/search?query=';
+
+module.exports = (req, res) => {
+
+  const { state } = req.query;
+  const url = `${SPACES_URL}${query}&state=${state || 'live'}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + process.env.BEARER,
+      'Content-Type': 'application/json'
+    },
+  });
+
+  const result = await response.json();
+
+  res.json({
+    spaces: result
+  })
+}
