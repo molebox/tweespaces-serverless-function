49ff4db2bcaf7cf072875f3018a38db68c571dd5
Richard Haines <hello@richardhaines.dev>
2021-09-04 09:40:10 +0200

    split up functions

--- api/spaces.js
+++ api/spaces.js
@@ -11,37 +11,67 @@ const cors = Cors({
 
 const SPACES_URL = 'https://api.twitter.com/2/spaces/search?query=';
 
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
 
-      return resolve(result)
-    })
-  })
+//       return resolve(result)
+//     })
+//   })
+// }
+
+
+async function getSpaces() {
+  const url = `${SPACES_URL}${query}&state=${state}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
+  try {
+    return await axios.get(url, { Authorization: `Bearer ${process.env.BEARER}` });
+
+  } catch (error) {
+    return error
+  }
+
 }
-
 
 export default async (req, res) => {
   const { body: { state, query } } = req;
   console.log({ state, query })
-  const url = `${SPACES_URL}${query}&state=${state}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
 
   try {
-    await runMiddleware(req, res, cors)
-    const spaces = await axios.get(url, { Authorization: `Bearer ${process.env.BEARER}` });
 
-
-    return res.send({
-      ...spaces
+    res.send({
+      status: 200,
+      spaces: await getSpaces()
     })
   } catch (error) {
-    return res.status(500).send({ message: 'Error, something went wrong: ', error })
+    res.send({
+      status: 500,
+      message: error.message,
+    })
   }
 
 }
+
+// export default async (req, res) => {
+//   const { body: { state, query } } = req;
+//   console.log({ state, query })
+//   const url = `${SPACES_URL}${query}&state=${state}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
+
+//   try {
+//     await runMiddleware(req, res, cors)
+//     const spaces = await axios.get(url, { Authorization: `Bearer ${process.env.BEARER}` });
+
+
+//     return res.send({
+//       ...spaces
+//     })
+//   } catch (error) {
+//     return res.status(500).send({ message: 'Error, something went wrong: ', error })
+//   }
+
+// }
 
   // const response = await fetch(url, {
   //   method: 'GET',
