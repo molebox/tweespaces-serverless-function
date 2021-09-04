8b9459b16b8a6bb0ff7d43ae81d36842e360b385
Richard Haines <hello@richardhaines.dev>
2021-09-04 20:47:07 +0200

    removed packages

--- api/spaces.js
+++ api/spaces.js
@@ -1,38 +1,14 @@
 require('dotenv').config();
-import fetch from 'node-fetch';
-import Cors from 'cors'
 import axios from 'axios'
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-})
-
-
-
 const SPACES_URL = 'https://api.twitter.com/2/spaces/search?query=';
-
-// function runMiddleware(req, res, fn) {
-//   return new Promise((resolve, reject) => {
-//     fn(req, res, (result) => {
-//       if (result instanceof Error) {
-//         return reject(result)
-//       }
-
-//       return resolve(result)
-//     })
-//   })
-// }
-
 
 async function getSpaces(query, state) {
   const url = `${SPACES_URL}${query}&state=${state}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
   try {
     const result = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
-    console.log('the result from twitter: ', result)
     return result
   } catch (error) {
-    console.log('the error from twitter: ', error)
     return error
   }
 
@@ -43,14 +19,11 @@ export default async (req, res) => {
 
   try {
     const result = await getSpaces(query, state)
-    console.log('result.data: ', result.data)
-    console.log('JSON.stringify(result.data): ', JSON.stringify(result.data))
     res.send({
       status: 200,
       spaces: result.data
     })
   } catch (error) {
-    console.log({ error })
     res.send({
       status: 500,
       message: error.message,
@@ -58,36 +31,3 @@ export default async (req, res) => {
   }
 
 }
-
-// export default async (req, res) => {
-//   const { body: { state, query } } = req;
-//   console.log({ state, query })
-//   const url = `${SPACES_URL}${query}&state=${state}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
-
-//   try {
-//     await runMiddleware(req, res, cors)
-//     const spaces = await axios.get(url, { Authorization: `Bearer ${process.env.BEARER}` });
-
-
-//     return res.send({
-//       ...spaces
-//     })
-//   } catch (error) {
-//     return res.status(500).send({ message: 'Error, something went wrong: ', error })
-//   }
-
-// }
-
-  // const response = await fetch(url, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Authorization': 'Bearer ' + process.env.BEARER,
-  //     'Content-Type': 'application/json'
-  //   },
-  // });
-
-  // const spaces = await response.json()
-
-  // return res.json({
-  //   spaces
-  // })
