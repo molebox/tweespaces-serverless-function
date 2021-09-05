7424812cb9633e2f9b7484e8fc144e67ae408517
Richard Haines <hello@richardhaines.dev>
2021-09-05 09:27:37 +0200

    loggin...12

--- api/space-by-user.js
+++ api/space-by-user.js
@@ -35,7 +35,7 @@ export default async (req, res) => {
         console.log({ result })
         res.send({
             status: 200,
-            spaces: user
+            spaces: user.data
         })
     } catch (error) {
         res.send({
