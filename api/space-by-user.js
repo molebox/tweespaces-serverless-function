61154e90622963bf53fe369bb3442c22f8f113df
Richard Haines <hello@richardhaines.dev>
2021-09-05 09:33:49 +0200

    loggin...15

--- api/space-by-user.js
+++ api/space-by-user.js
@@ -16,6 +16,7 @@ async function getUserIdByUsername(username) {
 }
 
 async function getSpaceByUser(id) {
+    console.log({ id })
     const url = `${SPACE_BY_USER_URL}${id}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
     try {
         const result = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
