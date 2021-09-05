54a846dfc5057e34e61e84ae80d51b5dfbb2e468
Richard Haines <hello@richardhaines.dev>
2021-09-05 09:19:24 +0200

    loggin...6

--- api/space-by-user.js
+++ api/space-by-user.js
@@ -31,11 +31,11 @@ export default async (req, res) => {
     try {
         const user = await getUserIdByUsername(username)
         console.log({ user })
-        const result = await getSpaceByUser(user.data.id)
+        const result = await getSpaceByUser(user.id)
         console.log({ result })
         res.send({
             status: 200,
-            spaces: result.data
+            spaces: user.data
         })
     } catch (error) {
         res.send({
