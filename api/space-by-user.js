cea51c2dac5eddc36b9a12121a977d3c57e67d42
Richard Haines <hello@richardhaines.dev>
2021-09-06 08:02:57 +0200

    more logging..17

--- api/space-by-user.js
+++ api/space-by-user.js
@@ -7,8 +7,8 @@ const SPACE_BY_USER_URL = `https://api.twitter.com/2/spaces/by/creator_id?user_i
 async function getUserIdByUsername(username) {
     const url = `${USER_BY_USERNAME_URL}${username}`;
     try {
-        const result = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
-        return result.data
+        const { data } = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
+        return data
     } catch (error) {
         return console.log('Get username error: ', error.message)
     }
@@ -18,8 +18,8 @@ async function getSpaceByUser(id) {
     const url = `${SPACE_BY_USER_URL}${id}&space.fields=participant_count,scheduled_start,title&expansions=creator_id&user.fields=name,description,username`;
 
     try {
-        const result = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
-        return result.data
+        const { data } = await axios.get(url, { headers: { 'Authorization': `Bearer ${process.env.BEARER}` } });
+        return data
     } catch (error) {
         return console.log('Get user space error: ', error.message)
     }
@@ -40,16 +40,16 @@ export default async (req, res) => {
     const { body: { username } } = req;
 
     try {
-        const user = await getUserIdByUsername(username)
+        await getUserIdByUsername(username).then(response => {
+            console.log({ response })
+            const { data } = await getSpaceByUser(response.id)
+            console.log('Get user space result: ', data)
 
-        const result = await getSpaceByUser(user.data.id)
-        console.log('Get user space result: ', result)
-
-        res.send({
-            status: 200,
-            spaces: result.data
+            res.send({
+                status: 200,
+                spaces: data
+            })
         })
-
 
     } catch (error) {
         res.send({
